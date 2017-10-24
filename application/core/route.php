fde6bf05420d17f041980136fc540661f8c274d6
charbel <charbelkisso@github.com>
2017-10-24 14:59:33 +0200

    remove comment

--- application/core/route.php
+++ application/core/route.php
@@ -1,16 +1,12 @@
 ﻿<?php
 
-/*
-Класс-маршрутизатор для определения запрашиваемой страницы.
-> цепляет классы контроллеров и моделей;
-> создает экземпляры контролеров страниц и вызывает действия этих контроллеров.
-*/
+
 class Route
 {
 
 	static function start()
 	{
-		// контроллер и действие по умолчанию
+
 		error_reporting(0);
 		$value = $_GET["id"];
 		switch ($value) {
@@ -42,20 +38,12 @@ class Route
 				break;
 		}
 
-		
 
-		// добавляем префиксы
+
+
 		$model_name = 'Model_'.$controller_name;
 		$controller_name = 'Controller_'.$controller_name;
 		$action_name = 'action_'.$action_name;
-
-		/*
-		echo "Model: $model_name <br>";
-		echo "Controller: $controller_name <br>";
-		echo "Action: $action_name <br>";
-		*/
-
-		// подцепляем файл с классом модели (файла модели может и не быть)
 
 		$model_file = strtolower($model_name).'.php';
 		$model_path = "application/models/".$model_file;
@@ -64,7 +52,7 @@ class Route
 			include "application/models/".$model_file;
 		}
 
-		// подцепляем файл с классом контроллера
+
 		$controller_file = strtolower($controller_name).'.php';
 		$controller_path = "application/controllers/".$controller_file;
 		if(file_exists($controller_path))
@@ -73,25 +61,22 @@ class Route
 		}
 		else
 		{
-			/*
-			правильно было бы кинуть здесь исключение,
-			но для упрощения сразу сделаем редирект на страницу 404
-			*/
+
 			Route::ErrorPage404('1');
 		}
 
-		// создаем контроллер
+
 		$controller = new $controller_name;
 		$action = $action_name;
 
 		if(method_exists($controller, $action))
 		{
-			// вызываем действие контроллера
+
 			$controller->$action($param);
 		}
 		else
 		{
-			// здесь также разумнее было бы кинуть исключение
+
 			Route::ErrorPage404('2');
 		}
 
@@ -101,8 +86,8 @@ class Route
 	{
         $host = 'http://'.$_SERVER['HTTP_HOST'].'/';
         header('HTTP/1.1 404 Not Found');
-		header("Status: 404 Not Found");
-		header('Location:'.$host.'404');
+				header("Status: 404 Not Found");
+				header('Location:'.$host.'404');
     }
 
 }
